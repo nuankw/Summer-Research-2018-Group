5d43b2b49398640f3fe25b78d331c9cc789b26f4
zhykoties <zhykoties@gmail.com>
2018-08-31 16:16:40 -0700

    Update rupture_p.py

--- statistics/rupture_p.py
+++ statistics/rupture_p.py
@@ -5,13 +5,10 @@ import data_helper
 
 training = True #change to False to run on the whole dataset
 
-shift_train_data = np.load("./data/elect_pre_train_data.npy")
-shift_train_onehot = np.load("./data/elect_train_onehot.npy")
-v_all = np.load("./data/elect_train_v.npy")
-shift_train_label = np.load("./data/elect_train_label.npy")
-param = np.load("./data/elect_train_param.npy")
-index_list = np.load("./data/elect_train_index.npy")
-indexs_pred = np.load("./data/elect_train_pred_index.npy")
+if training:
+    shift_train_data = np.load("./data/elect_train_data.npy")
+else:
+    shift_train_data = np.load("./data/elect_data.npy")
 
 shift_train_pvalue = [] #change points for each window
 
@@ -25,11 +22,12 @@ for i in range(shift_train_data.shape[0]):
     sigma = np.std(signal)
     my_bkps = algo.predict(pen=2*np.log(n)*dim*sigma**2)
     for j in my_bkps:
-        if j < 160:
+        if j < 160: #don't label as a change point if it occurs too late in the series
             change_points[j-1] = 1
     # save results
     shift_train_pvalue.append(change_points)
 shift_train_pvalue = np.array(shift_train_pvalue)
+
 if(training):
     np.save("./data/elect_train_p_value", shift_train_pvalue)
 else:
