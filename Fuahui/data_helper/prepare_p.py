5a67de9997301352de9a760a2cd031a214096e3e
SC12138 <song.chen.628@gmail.com>
2018-09-02 00:10:23 -0700

    Reconstruct all current models

---
+++ Fuahui/data_helper/prepare_p.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pylab as plt
+import ruptures as rpt
+
+
+
+shift_train_data = np.load("../data/Electricity/elect_pre_train_data.npy")
+shift_train_onehot = np.load("../data/Electricity/elect_train_onehot.npy")
+v_all = np.load("../data/Electricity/elect_train_v.npy")
+shift_train_label = np.load("../data/Electricity/elect_train_label.npy")
+param = np.load("../data/Electricity/elect_train_param.npy")
+index_list = np.load("../data/Electricity/elect_train_index.npy")
+indexs_pred = np.load("../data/Electricity/elect_train_pred_index.npy")
+
+shift_train_pvalue = [] #change points for each window
+
+n, dim = 192, 1  # number of samples, dimension
+model = "rbf"  # "l1", "rbf", "linear", "normal", "ar"
+for i in range(shift_train_data.shape[0]):
+    change_points = np.zeros(192)
+    signal = shift_train_data[i, :, 0]
+    # change point detection
+    algo = rpt.Window(width=48, model=model).fit(signal[:-24])
+    sigma = np.std(signal)
+    my_bkps = algo.predict(pen=2*np.log(n)*dim*sigma**2)
+    for j in my_bkps:
+        if j < 160:
+            change_points[j-1] = 1
+    # save results
+    shift_train_pvalue.append(change_points)
+shift_train_pvalue = np.array(shift_train_pvalue)
+
+np.save("../data/Electricity/elect_train_p_value", shift_train_pvalue)
---
+++ Fuahui/readme.md
@@ -0,0 +1,9 @@
+Committed by Chen, Yunkai and Nuan. 
+
+Implementation of pure DeepAR and DeepAR with Dilated CNN. 
+
+Data_helpers preprocess the raw data and save them by windows in .npy format.
+
+rnn_cell_impls are modification of LSTM_Cell's source code in Tensorflow according to different models. Make sure to duplicate the original one before replacing it.
+
+Load_DeepAR loads trained models and draw images of specified windows.  
